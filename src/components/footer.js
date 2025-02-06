c6f223419eac0c482d8e5b30fb37d8549dfc6d5e
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-02-07 02:35:32 +0500

    fix: adjust footer padding and layout for improved alignment and responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,13 +17,13 @@ export default function Footer() {
   //   "https://lh3.googleusercontent.com/pw/AP1GczN9HraQsHh0pJ0YoBThzcS0tY_-uk5zyQquRDFTVgpN6peh1An56u-fUREcdmwVRl0gE_E7MGnSXH_Zsj-fjMW8nIFHQ8lKIep5Xwi6TKKsPZa7kyQLrqofq3dw8994xfpRNfHIjbG257eM0pMU9f4U=w658-h263-s-no-gm";
 
   return (
-    <div className="flex w-full flex-col items-center px-3 md:px-6">
+    <div className="flex w-full flex-col items-center p-3 md:px-6">
       <div className="flex flex-col justify-center w-full max-w-[1200px]">
-        <div className="flex flex-col md:flex-row gap-8 md:gap-16  md:justify-evenly items-center md:items-end w-full p-4 px-2 md:px-6 ">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-16 justify-center md:justify-evenly items-center md:items-end w-full p-4 px-2 md:px-6 ">
           <div className="flex flex-col items-center justify-center md:pb-3">
             <img src={logoLink} alt="logo" className="h-[50px] md:h-[70px]" />
           </div>
-          <div className="flex flex-col w-full md:w-auto justify-center gap-3 text-[var(--text-prim)] text-[12px] md:text-[14px] ">
+          <div className="flex flex-col min-w-[90%] bg-red-200 md:w-auto justify-center gap-3 text-[var(--text-prim)] text-[12px] md:text-[14px] ">
             <div className="flex gap-6 md:gap-32 justify-center">
               <div className="flex flex-col w-full md:w-auto gap-3">
                 <div className="flex items-center gap-3">
