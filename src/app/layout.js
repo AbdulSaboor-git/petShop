a33e23ea91c2a482d4f84343956e711a1c20b164
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-02-15 23:22:35 +0500

    feat: remove service worker and login page components to streamline application

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,22 +56,6 @@ export default function RootLayout({ children }) {
     }
   }, []);
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    }
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
