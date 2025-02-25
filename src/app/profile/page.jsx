3bec0cd941e820e0d1adebc970c59170fd3a96c9
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-02-25 21:36:09 +0500

    feat: enhance profile page layout and display seller information with improved styling

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,7 +9,7 @@ export default function Profile() {
   const [seller, setSeller] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const sellerName = (seller?.firstName + " " + seller?.lastName).toUpperCase();
   useEffect(() => {
     if (typeof window != "undefined") {
       const params = new URLSearchParams(window.location.search);
@@ -38,7 +38,7 @@ export default function Profile() {
   }, [id]);
 
   return (
-    <div className="flex bg-gray-100 flex-col gap-4 lg:gap-6 items-center">
+    <div className="flex flex-col gap-4 lg:gap-6 items-center">
       <Header />
       <div className="flex flex-col items-center justify-center w-full max-w-[1400px] px-5">
         {loading ? (
@@ -50,7 +50,19 @@ export default function Profile() {
             {error}
           </div>
         ) : (
-          <h1>{seller.firstName + " " + seller.lastName}</h1>
+          <div className="w-full flex flex-col gap-4 items-center justify-start h-screen bg-gray-100 rounded-2xl mt-[52px] md:mt-[60px]">
+            <div className="flex gap-6 items-center justify-start bg-gray-200 p-3 w-[85%] md:max-w-[600px]  rounded-2xl -mt-[52px] md:-mt-[60px]">
+              <img
+                src={seller.profilePicture}
+                alt={seller.firstName}
+                className="h-20 md:h-24 aspect-square rounded-xl"
+              />
+              <div className="text-xs md:text-sm ">
+                <div className="font-extrabold">{sellerName}</div>
+                <div>Total Products: {seller.items.length}</div>
+              </div>
+            </div>
+          </div>
         )}
       </div>
 
