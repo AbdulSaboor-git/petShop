642668e8a12df9b1ba2f43bf67e4206f8ade52cb
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-02-26 01:45:06 +0500

    refactor: rename ProfilePage component to Profile and simplify profile display logic

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,9 +6,8 @@ import ProductCard_S from "@/components/productCard_S";
 import React, { useEffect, useState } from "react";
 import useAuthUser from "@/hooks/authUser";
 import { MdAccountCircle } from "react-icons/md";
-import Profile from "@/components/profile";
 
-export default function ProfilePage() {
+export default function Profile() {
   const [id, setId] = useState(null);
   const [seller, setSeller] = useState(null);
   const [items, setItems] = useState([]);
@@ -16,13 +15,10 @@ export default function ProfilePage() {
   const [premiumItems, setPremiumItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const [showAcc, setShowAcc] = useState(false);
-  const [profileMounted, setProfileMounted] = useState(false);
-  const [profileAnimClass, setProfileAnimClass] = useState("");
-
   const [UserIsSeller, setUserIsSeller] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const { user, userLoading, logout } = useAuthUser();
+
   const sellerName = (seller?.firstName + " " + seller?.lastName).toUpperCase();
 
   useEffect(() => {
@@ -33,6 +29,10 @@ export default function ProfilePage() {
     }
   }, [user]);
 
+  function toggleShowProfile() {
+    setShowProfile((prev) => !prev);
+  }
+
   useEffect(() => {
     if (typeof window != "undefined") {
       const params = new URLSearchParams(window.location.search);
@@ -40,27 +40,6 @@ export default function ProfilePage() {
       if (id && id != "undefined") setId(id);
     }
   }, []);
-
-  const toggleShowAcc = () => {
-    setShowAcc((prev) => !prev);
-  };
-  const closeShowAcc = () => {
-    setShowAcc(false);
-  };
-
-  // Manage mounting and animation for Profile popup
-  useEffect(() => {
-    if (showAcc) {
-      setProfileMounted(true);
-      setProfileAnimClass("animate-openAccPopUp");
-    } else if (profileMounted) {
-      setProfileAnimClass("animate-closeAccPopUp");
-      const timer = setTimeout(() => {
-        setProfileMounted(false);
-      }, 300);
-      return () => clearTimeout(timer);
-    }
-  }, [showAcc]);
 
   useEffect(() => {
     if (id === null || id === "undefined") return;
@@ -100,52 +79,68 @@ export default function ProfilePage() {
   return (
     <div className="flex flex-col gap-4 lg:gap-6 items-center">
       <Header />
-      <div className="flex flex-col items-center justify-center w-full max-w-[1400px] px-5">
+      <div className="flex flex-col items-center justify-center w-full max-w-[1400px]">
         {loading ? (
-          <div className="h-screen pt-6">
+          <div className="h-screen pt-6 px-5">
             <Loader />
           </div>
         ) : error ? (
-          <div className="h-screen text-sm md:text-base text-gray-500 p-2  self-start">
+          <div className="h-screen text-sm md:text-base text-gray-500 p-2 self-start px-5">
             {error}
           </div>
         ) : (
-          <div className="w-full flex flex-col gap-12 pt-0 p-4 justify-start bg-gray-100 rounded-2xl mt-[52px] md:mt-[60px]">
-            <div className="flex gap-4 mdd:gap-6 self-center items-center justify-start bg-gray-200 p-3 w-[93%] md:max-w-[600px]  rounded-2xl -mt-[52px] md:-mt-[60px] ">
-              <img
-                src={seller.profilePicture}
-                alt={seller.firstName}
-                className="h-20 md:h-24 aspect-square rounded-xl"
-              />
-              <div className="flex flex-col text-xs md:text-sm  ">
-                <div className="font-extrabold">{sellerName}</div>
-                <div>Total Products: {items.length}</div>
-                {user && UserIsSeller && (
-                  <div className="relative z-20 flex items-center justify-center">
+          <div className="w-full flex flex-col gap-12 pt-0 p-5 justify-start bg-gray-100 rounded-2xl mt-[52px] md:mt-[60px]">
+            <div className="flex flex-col items-center justify-start self-center bg-gray-200 p-3 w-[93%] md:max-w-[600px]  rounded-2xl -mt-[52px] md:-mt-[60px] overflow-hidden">
+              <div className="flex gap-4 md:gap-6 self-start items-center justify-start">
+                <img
+                  src={seller.profilePicture}
+                  alt={seller.firstName}
+                  className="h-20 md:h-24 aspect-square rounded-xl"
+                />
+                <div className="flex flex-col text-xs md:text-sm  ">
+                  <div className="font-extrabold">{sellerName}</div>
+                  <div>Total Products: {items.length}</div>
+                  {user && UserIsSeller && (
                     <button
-                      onClick={toggleShowAcc}
-                      className="mt-1 z-20 flex gap-2 items-center justify-center w-full bg-gradient-to-br from-green-500 to-green-600 hover:bg-gradient-radial rounded-md p-0.5 md:p-1 px-2 text-white"
+                      onClick={toggleShowProfile}
+                      className="mt-1 flex gap-2 items-center justify-center w-full bg-gradient-to-br from-green-500 to-green-600 hover:bg-gradient-radial rounded-md p-0.5 md:p-1 px-2 text-white"
                     >
                       <MdAccountCircle /> Profile
                     </button>
-                    {/* Profile popup */}
-                    {profileMounted && (
-                      <div
-                        className={`absolute z-10 backdrop-blur-[8px] top-8 rounded-xl md:mr-10 ${
-                          user?.role === "SELLER" && "lg:right-8 md:mr-5"
-                        } ${profileAnimClass}`}
-                      >
-                        <Profile close={closeShowAcc} />
-                      </div>
-                    )}
-                  </div>
-                )}
+                  )}
+                </div>
+              </div>
+              <div
+                className={`transition-all duration-300 ease-in-out overflow-hidden text-xs md:text-sm bg-white rounded-xl
+                ${
+                  showProfile
+                    ? "max-h-[200px] opacity-100 m-3 p-4"
+                    : "max-h-0 opacity-0 m-0 p-0 "
+                }
+                `}
+              >
+                <div className="">
+                  <strong> Name:</strong> {seller.firstName} {seller.lastName}
+                  <br />
+                  <strong> Email:</strong> {seller.email}
+                  <br />
+                  <strong> Contact:</strong> {seller.phoneNo}
+                  <br />
+                  <strong> Role:</strong> {seller.role.toLowerCase()}
+                  <br />
+                  <button
+                    onClick={logout}
+                    className="mt-1 flex gap-2 items-center justify-center w-full bg-gradient-to-br from-red-500 to-red-600 hover:bg-gradient-radial rounded-md p-0.5 md:p-1 px-2 text-white"
+                  >
+                    Logout
+                  </button>
+                </div>
               </div>
             </div>
             {premiumItems.length > 0 && (
               <div className="flex flex-col gap-2">
                 <h1 className="font-bold">Featured Products</h1>
-                <div className="flex gap-3 w-full justify-start overflow-auto hidden_scroll_bar">
+                <div className="flex gap-4 w-full justify-start overflow-auto hidden_scroll_bar">
                   {featuredItems.map((item) => (
                     <ProductCard_S key={item.id} item={item} alt={true} />
                   ))}
@@ -155,7 +150,7 @@ export default function ProfilePage() {
             {premiumItems.length > 0 && (
               <div className="flex flex-col gap-2">
                 <h1 className="font-bold">Premium Products</h1>
-                <div className="flex gap-3 w-full overflow-auto hidden_scroll_bar">
+                <div className="flex gap-4 w-full overflow-auto hidden_scroll_bar">
                   {premiumItems.map((item) => (
                     <ProductCard_S key={item.id} item={item} alt={true} />
                   ))}
